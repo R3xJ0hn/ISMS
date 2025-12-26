262312f0d15ccc9f9c1128936398bb60b5927cfc
R3xJ0hn <pogirawsirex@gmail.com>
2025-12-26 20:26:18 +0800

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Welcome to the ISMS Application</h1>
+      <p>This is a placeholder for the main page content.</p>
+    </div>
+  );
+}
